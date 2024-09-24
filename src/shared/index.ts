a2b9863640cedc69edad9805dee305f977bd7993
Dmytry64 <nosdm64@gmail.com>
2024-09-24 09:13:38 +0300

    feat: добавляет интерфейс IOfferGenerator

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,3 +1,4 @@
 import { TVSFileReader } from './libs/file-reader/tsv-file-reader.js';
+import { IMockServerData } from './types/mock-server-data.type.js';
 
-export {TVSFileReader};
+export {TVSFileReader, IMockServerData};
---
+++ src/shared/libs/offer-generator/index.ts
@@ -0,0 +1,3 @@
+import { IOfferGenerator } from './offer-generator.interface.js';
+
+export {IOfferGenerator};
