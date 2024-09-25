68313e925f95d3d5aadd46e3fdbb2e16f693f110
Dmytry64 <nosdm64@gmail.com>
2024-09-25 13:35:42 +0300

    feat: добавляет generate.command.ts

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,6 +1,9 @@
 import got from 'got';
+import { appendFile } from 'node:fs/promises';
+
 import { IMockServerData } from '../../shared/index.js';
 import { Command } from './command.interfae.js';
+import { TSVOfferGenerator } from '../../shared/libs/offer-generator/tsv-offer-generator.js';
 
 export class GenerateCommand implements Command {
   private initialData: IMockServerData;
@@ -8,8 +11,17 @@ export class GenerateCommand implements Command {
   private async load(url: string) {
     try {
       this.initialData = await got.get(url).json();
+
     } catch {
       throw new Error(`Can't load data from ${url}`);
+    }
+  }
+
+  private async write(filepath: string, offerCount: number) {
+    const tvsOfferGenerator = new TSVOfferGenerator(this.initialData);
+    for (let i = 0; i < offerCount; i++) {
+      const row = `${tvsOfferGenerator.generate()}\n`;
+      await appendFile(filepath, row, {encoding: 'utf-8'});
     }
   }
 
@@ -22,6 +34,8 @@ export class GenerateCommand implements Command {
     const offerCount = Number.parseInt(count, 10);
     try {
       await this.load(url);
+      await this.write(filepath, offerCount);
+      console.info(`File ${filepath} was created`);
     } catch (error: unknown) {
       console.error('Can\'t generate data');
 
